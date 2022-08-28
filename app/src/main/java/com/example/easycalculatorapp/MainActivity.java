383b71c43ec9de3f549a00a8f87831978231aa76
skarandeep <108695031+skarandeep@users.noreply.github.com>
2022-08-28 18:23:44 -0400

    Commit-2 Assigned IDs to all Image and TextView

--- app/src/main/java/com/example/easycalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/easycalculatorapp/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.easycalculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //creating objects for all buttons
+    ImageView btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9, btn_0;
+    ImageView btn_dot, btn_equal, btn_ac, btn_module, btn_plus, btn_minus, btn_multiplication, btn_divide;
+
+    TextView inputText, outputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //assigning id for each ImageView
+        btn_0 = findViewById(R.id.btn_0);
+        btn_1 = findViewById(R.id.btn_1);
+        btn_2 = findViewById(R.id.btn_2);
+        btn_3 = findViewById(R.id.btn_3);
+        btn_4 = findViewById(R.id.btn_4);
+        btn_5 = findViewById(R.id.btn_5);
+        btn_6 = findViewById(R.id.btn_6);
+        btn_7 = findViewById(R.id.btn_7);
+        btn_8 = findViewById(R.id.btn_8);
+        btn_9 = findViewById(R.id.btn_9);
+
+        btn_dot = findViewById(R.id.btn_dot);
+        btn_equal = findViewById(R.id.btn_equal);
+        btn_ac = findViewById(R.id.btn_ac);
+        btn_module = findViewById(R.id.btn_module);
+        btn_plus = findViewById(R.id.btn_plus);
+        btn_minus = findViewById(R.id.btn_minus);
+        btn_multiplication = findViewById(R.id.btn_multiplication);
+        btn_divide = findViewById(R.id.btn_divide);
+
+        inputText = findViewById(R.id.inputText);
+        outputText = findViewById(R.id.outputText);
+
     }
 }
